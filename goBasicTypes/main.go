1f250722073686e677f097f66bf187cedb7c6cfc
AkiebNazir <shahzada.njasm@gmail.com>
2023-08-03 16:47:36 +0530

    array and slices with receiver function

--- goBasicTypes/main.go
+++ goBasicTypes/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/goProjects/goBasicTypes/arrayslices"
 	"github.com/goProjects/goBasicTypes/parseJson"
 )
 
@@ -12,5 +13,19 @@ func main() {
 	// parsing json data from a file
 
 	fmt.Printf("%+v\n", parseJson.ParseJsonData("./parseJson/data.json"))
+
+	// array and slices in god
+
+	var arr arrayslices.ArrayNums
+	var sl arrayslices.SliceNums
+	for i := 1; i < 21; i++ {
+		arr.Arr[i-1] = i
+		sl.Arr = append(sl.Arr, i)
+	}
+	arr.ArrayMonupulation()
+	sl.SliceEdit()
+
+	fmt.Printf("the array elements are :%v\n", arr)
+	fmt.Printf("the slice elements are :%v\n", sl)
 
 }
