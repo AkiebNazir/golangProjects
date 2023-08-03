0bdd54f27f3d4a73a9398821cd22d4e33a83e519
AkiebNazir <shahzada.njasm@gmail.com>
2023-08-03 16:05:14 +0530

    add go mod

--- goBasicTypes/go.mod
+++ goBasicTypes/go.mod
@@ -1,5 +1,3 @@
 module github.com/goProjects/goBasicTypes
 
 go 1.20
-
-require golang.org/x/example/hello v0.0.0-20230802215733-17e2836bb62e
--- goBasicTypes/main.go
+++ goBasicTypes/main.go
@@ -4,14 +4,10 @@ import (
 	"fmt"
 
 	"github.com/goProjects/goBasicTypes/parseJson"
-	"github.com/goProjects/goBasicTypes/stringGo"
-	"golang.org/x/example/hello/reverse"
 )
 
 func main() {
 	fmt.Println("module basic types")
-	fmt.Println(reverse.String("hello"))
-	stringGo.CustomerStrings(20, 400)
 
 	// parsing json data from a file
 
